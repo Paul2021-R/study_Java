cccf294584cc1b1d9b8ab4e12bcf7f8365741b76
류한솔 <ryuhansol@lyuhansols-MacBook-Pro.local>
2023-04-13 17:43:48 +0900

    START: JAVA study

--- hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
+		// 스프링 어플리케이션을 실행 -> 자체 내장 서버를 가동시켜서 TOMCAT을 통해 서버가 가동된다.
 		SpringApplication.run(HelloSpringApplication.class, args);
+
 	}
 
 }
